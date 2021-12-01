4cbe1d1522e0bd01cc4bd43090ca1c3796ac6452
KLAUS WONG (SYS-TSV-ISD-OOCLL/HKG) <WONGKL2@OOCL.COM>
2021-12-01 17:26:58 +0800

    [OOPHW][KLAUSWONG-2] feat: practice03 submission

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,17 @@
 package practice03;
 
-public class Student {
+public class Student extends Person{
+    private final int classNumber;
+    public Student(String name, int age, int classNumber) {
+        super(name, age);
+        this.classNumber = classNumber;
+    }
+
+    public Integer getKlass() {
+        return this.classNumber;
+    }
+
+    public String introduce() {
+        return String.format("I am a Student. I am at Class %d.",this.classNumber);
+    }
 }
