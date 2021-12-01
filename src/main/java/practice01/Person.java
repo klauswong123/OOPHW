291ca3344650c39ee0008499cb1feef22e07af7b
KLAUS WONG (SYS-TSV-ISD-OOCLL/HKG) <WONGKL2@OOCL.COM>
2021-12-01 17:19:13 +0800

    [OOPHW][KLAUSWONG-2] feat: practice02 submission

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -17,6 +17,6 @@ public class Person {
     }
 
     public String introduce() {
-        return String.format("My name is %s. I am %d years old.",name,age);
+        return String.format("My name is %s. I am %d years old.",this.name,this.age);
     }
 }
